6b26b80612778ad759fb232310db1f88a4fde528
elianiva <dicha.arkana03@gmail.com>
2023-04-23 21:23:34 +0700

    feat: add footer

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,9 +7,7 @@ export function Header() {
 	return (
 		<header className="p-4 shadow-md w-full sticky left-0 right-0 top-0 z-40 bg-white">
 			<div className="mx-auto max-w-screen-2xl grid grid-cols-[1fr,4fr,1fr] gap-8">
-				<div>
-					<Image src="/assets/wri-logo.png" alt="WRI Logo" width={200} height={64} />
-				</div>
+				<Image src="/assets/wri-logo.png" alt="WRI Logo" width={200} height={64} />
 				<div className="flex items-center justify-center gap-4">
 					{MENU_ITEMS.map((menu) => (
 						<Menu key={menu.href} href={menu.href}>
