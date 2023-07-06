d500b8623f3700f2fe9c4c3fbaf31ac24f76ba5d
elianiva <dicha.arkana03@gmail.com>
2023-07-06 17:43:54 +0700

    chore: setup eslint and prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	integrations: [
-		tailwind(),
-		svelte(),
-	],
+	integrations: [tailwind(), svelte()],
 	vite: {
 		plugins: [
 			Icons({
