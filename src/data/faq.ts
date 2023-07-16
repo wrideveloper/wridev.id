a3701042ca33fbb1a7248a0174ca102182d0f00c
elianiva <dicha.arkana03@gmail.com>
2023-07-16 21:47:24 +0700

    chore: shorten the faq

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -15,20 +15,19 @@ export const FAQ_DATA: FaqData = {
 		{
 			question: "Kapan bakal ada open recruitment?",
 			// eslint-disable-next-line quotes
-			answer: 'Nah, open recruitment biasanya digelar di awal tahun ajaran baru, kira-kira bulan September gitu. Yang seru, acara ini terbuka buat siapa aja, termasuk mahasiswa baru yang belum punya pengalaman riset informatika sebelumnya. Kalau pengen tau info terbaru tentang open recruitment, bisa cek Instagram WRI kita ya. Sebelum open recruitment, biasanya juga ada acara "Early Access" yang isinya pengenalan WRI. Jangan lupa keep update terus akun media sosial kita biar ga ketinggalan info, guys!',
+			answer: 'Open recruitment WRI biasanya diadakan di awal tahun ajaran baru, sekitar bulan September. Acara ini terbuka untuk siapa saja, termasuk mahasiswa baru yang belum memiliki pengalaman riset informatika sebelumnya. Jangan lupa pantau akun Instagram WRI untuk informasi terbaru dan ikuti acara "Early Access" untuk pengenalan WRI.',
 		},
 		{
 			question: "Early Access itu acara apa sih?",
-			answer: "Oke, jadi Early Access itu sejenis acara buat kalian kenalan sama Workshop Riset Informatika (WRI) kita. Acaranya casual banget, kita bakal santai-santai doang sambil ngobrol-ngobrol santai. Kalian juga bisa tanya-tanya apa aja tentang WRI, ga ada yang dilarang, kok. Biasanya di acara ini juga bakal ada kuis unik dengan hadiah yang menarik, jadi jangan sampai kelewatan, ya!",
+			answer: "Jadi Early Access tuh acara casual buat kenalan sama WRI kita. Kita bisa santai-santai ngobrol dan tanya-tanya tentang WRI, seru banget deh! Jadi jangan sampai ketinggalan!",
 		},
 		{
 			question: "Acara yang ada di WRI itu apa aja?",
-			// eslint-disable-next-line quotes
-			answer: 'Wah, di WRI ada banyak acara seru yang bisa kamu ikutan, guys! Pertama, kita punya miniclass, mirip-mirip kayak kelas di kampus tapi lebih santai. Ada 4 miniclass, yaitu web development, mobile app development, game development, dan ui/ux design. Selain itu, kita juga sering adain acara open talks yang mirip kayak seminar gitu. Nah, selain itu, ada juga acara WRI OOT, singkatan dari "WRI Out of Topic". Nah, acara ini tuh santai banget, ngobrol-ngobrol tentang hal-hal random seputar IT, seru banget deh!',
+			answer: "Di WRI banyak acara seru yang bisa kamu ikutan, guys! Ada miniclass yang lebih santai daripada kelas kampus, ada 4 pilihan topik: web development, mobile app development, game development, dan ui/ux design. Selain itu, ada juga acara open talks mirip seminar, dan acara WRI OOT yang santai banget buat ngobrol-ngobrol tentang hal-hal random seputar IT, seru banget deh!",
 		},
 		{
 			question: "Siapa yg bakal ngajar di miniclass?",
-			answer: "Tiap miniclass bakal punya mentornya masing masing. Nah, mentor-mentor ini sebenarnya adalah mahasiswa yang punya keahlian dan pengalaman di bidang tersebut, jadi bisa belajar langsung dari orang yang punya pengalaman, pastinya lebih asik dan santai. Kalau kamu juga punya keahlian dan pengalaman di bidang IT tertentu dan pengen jadi mentor, boleh banget lho kasih tau kita! Kami sangat terbuka untuk orang-orang berbakat seperti kamu.",
+			answer: "Di tiap miniclass bakal ada mentornya sendiri-sendiri, guys. Nah, mentornya ini sebenarnya mahasiswa yang udah punya pengalaman di bidang itu, jadi belajarnya lebih asik dan santai. Kalo kamu juga punya keahlian di bidang IT tertentu dan pengen jadi mentor, boleh banget lho kasih tau kita! Kami selalu terbuka buat orang-orang berbakat kayak kamu.",
 		},
 	],
 };
