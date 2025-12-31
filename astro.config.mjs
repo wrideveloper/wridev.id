337e7220644e606cc07c0238609ef4d82c4dee92
Nobberu <itsnobberu@gmail.com>
2025-12-31 16:55:45 +0700

    Hero Section & Style Progress
    - Added required fonts.
    - Import package for phosphor icons (use "ph:" in icon names to access phosphor icons).

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import svelte from "@astrojs/svelte";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), icon()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
