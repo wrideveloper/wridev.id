f70d8a5c84ba9fac2ec819a5f5b8e869d98263f5
FlaBBB <f12345ff67@gmail.com>
2025-12-31 20:17:37 +0700

    feat: adding inline icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
