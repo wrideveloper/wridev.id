37b2e24695d7ce321096b5c6620b3e9a93be5623
FlaBBB <f12345ff67@gmail.com>
2026-01-01 12:35:30 +0700

    feat: adding testimonial section.

--- src/components/sections/tech/components/walking-logo.svelte
+++ src/components/sections/tech/components/walking-logo.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
-  const logos = [
-    '/assets/technology/blender.webp',
-    '/assets/technology/figma.webp',
-    '/assets/technology/laravel.webp',
-    '/assets/technology/svelte.webp',
-    '/assets/technology/flutter.webp',
-    '/assets/technology/aseprite.webp',
-    '/assets/technology/nodejs.webp',
-    '/assets/technology/unity.webp',
-  ];
+  export let logos: string[];
 </script>
 
 <div
   class="w-full overflow-hidden bg-white
   [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]"
 >
-  <div class="flex w-max animate-walk">
+  <div class="flex w-max animate-walk-left">
     {#each [...logos, ...logos] as logo}
       <img
         src={logo}
---
+++ src/components/sections/testimonial/components/card.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    const colorClass = {
+      "wri-yellow": {
+        div: "bg-[#FEF4DC]",
+        text: "inline-block text-transparent bg-gradient-to-b from-[#FFB12B] to-[#FFB12B]/56 bg-clip-text"
+      },
+      "wri-blue": {
+        div: "bg-[#D7EFFF]",
+        text: "text-[#0B79FF]"
+      },
+    }
+
+    type colorType = keyof typeof colorClass;
+
+    export let name: string;
+    export let title: string;
+    export let description: string;
+    export let color: colorType = "wri-yellow";
+
+    let divClass = colorClass[color].div;
+    let textClass = colorClass[color].text;
+</script>
+
+<div class={`w-100 h-50 px-6 py-3 rounded-2xl flex flex-col border-4 border-white/24 text ${divClass}`}>
+    <div>
+        <p class={`text-3xl font-semibold ${textClass}`}>{title}</p>
+        <p class={`${textClass}`}>{description}</p>
+    </div>
+    <p class={`mt-auto ${textClass}`}>- {name}</p>
+</div>
