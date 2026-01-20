0ff64b6e0da168d83fb8bc5e05586dbc8be60f8d
FlaBBB <f12345ff67@gmail.com>
2026-01-20 10:51:33 +0700

    feat: Switch Astro adapter from Node.js to Cloudflare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
 import uicons from "unplugin-icons/vite";
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
+
   integrations: [svelte(), icon()],
+
   vite: {
     plugins: [tailwindcss(), uicons({ compiler: "svelte" })],
   },
