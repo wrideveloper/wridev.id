0f7c7ea267701475e28a9bbbf99e43210f0dbfc5
FlaBBB <f12345ff67@gmail.com>
2026-02-03 13:47:11 +0700

    fix: implement correct sizing.

--- src/components/sections/feature/components/stats-pool.svelte
+++ src/components/sections/feature/components/stats-pool.svelte
@@ -8,19 +8,19 @@
     }[];
 </script>
 
-<div class="flex flex-col md:flex-row w-full gap-10 md:gap-0 md:justify-between items-start md:items-center">
+<div class="flex flex-col md:flex-row w-full gap-10 md:gap-0 md:justify-between items-center md:items-start">
     {#each stats as { title, value, desc }, index}
-        <div class="flex flex-col md:flex-row items-start md:items-center w-full md:w-auto">
+        <div class="flex flex-col md:flex-row items-center md:items-center w-full md:w-auto">
             {#if index !== 0}
                 <div
                     class="hidden md:block w-px bg-linear-to-b from-white via-wri-blue to-white h-[9rem] shrink-0 mr-12"
                 ></div>
             {/if}
 
-            <div class="flex flex-col gap-3 items-start">
+            <div class="flex flex-col gap-3 items-center md:items-start">
                 <PillsBlueText>{title}</PillsBlueText>
                 <div class="text-6xl font-bold">{value}</div>
-                <div class="text-gray-600 w-full md:max-w-100 text-lg">{desc}</div>
+                <div class="text-gray-600 w-full md:max-w-100 text-lg text-center md:text-left">{desc}</div>
             </div>
         </div>
     {/each}
